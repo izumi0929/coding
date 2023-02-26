96d0c32d5cbea9fc5ed5cfd4e104e94764652e62
izumeeeel <izumi.watahiki@gmail.com>
2023-02-26 18:00:36 +0900

    feat: add api get and post

--- books/security-handson/server.js
+++ books/security-handson/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const api = require("./routes/api")
 const app = express()
 const port = 3002
 
 app.use(express.static("public"))
+
+app.use("/api", api)
 
 app.get("/", (req, res) => {
   res.end("Hello World!")
