85551a51298c77aa7ba04d721df9844298daa3da
izumeeeel <izumi.watahiki@gmail.com>
2023-02-26 19:40:47 +0900

    feat: 任意のヘッダ追加

--- books/security-handson/routes/api.js
+++ books/security-handson/routes/api.js
@@ -2,7 +2,18 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.send({ message: "Hello World!" })
+  res.setHeader("X-Timestamp", Date.now())
+  let message = req.query.message
+  const lang = req.headers["x-lang"]
+  if (message === "") {
+    res.status(400)
+    if (lang === "en") {
+      message = "message is empty"
+    } else {
+      message = "メッセージが空です"
+    }
+  }
+  res.send({ message })
 })
 
 router.use(express.json())
