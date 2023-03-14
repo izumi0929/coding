1b93970a501a4bde8b13fb6ab1a3cac1a1b403ce
izumeeeel <izumi.watahiki@gmail.com>
2023-03-15 08:52:28 +0900

    feat: hello csp

--- books/security-handson/server.js
+++ books/security-handson/server.js
@@ -3,12 +3,18 @@ const api = require("./routes/api")
 const app = express()
 const port = 3002
 
+app.set("view engine", "ejs")
+
 app.use(express.static("public"))
 
 app.use("/api", api)
 
 app.get("/", (req, res) => {
   res.end("Hello World!")
+})
+
+app.get("/csp", (req, res) => {
+  res.render("csp")
 })
 
 app.listen(port, () => {
